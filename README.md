51eca6965501f2246a6d9b91e686fc32b4d3d01f
Charles Oliveira <105311867+OliveiraCharles@users.noreply.github.com>
2023-09-28 21:55:51 -0300

    Improved Grafo

--- README.md
+++ README.md
@@ -6,18 +6,16 @@
 2. Enumerar o código
 3. Grafo
 
-      [1]
-   /  | |  \
-[2] [3] [4] [5]
-   \  | |  /
-      [6]
+    |Grafo|Legenda|
+    |-|-|
+    |![Grafo](img/grafo.excalidraw.svg)|    - [1]: Coleta da idade do funcionário</br> - [2]: Caso tenha menos de 16 anos de idade</br> - [3]: Caso tenha entre 16 e 18 anos de idade</br> - [4]: Caso tenha entre 19 e 55 anos de idade</br> - [5]: Caso tenha mais de 55 anos de idade</br> - [6]: Fim do Programa|
 
 4. Determinar Caminhos Básicos
 
-    1. 1 - 2 - 6
-    2. 1 - 3 - 6
-    3. 1 - 4 - 6
-    4. 1 - 5 - 6
+    - 1. 1 - 2 - 6
+    - 2. 1 - 3 - 6
+    - 3. 1 - 4 - 6
+    - 4. 1 - 5 - 6
 
 5. Descrever Caminhos
 6. Determinar Qt de Caminhos
@@ -29,6 +27,8 @@
     - Técnica de particionamento por Valor Limite
 10. Verificar Resultados no JaCoCo (Relatório)
 
+## Checklist
+
 - [x] Análise do código
 - [x] Enumerar o código
 - [x] Grafo
@@ -39,3 +39,5 @@
 - [ ] Determinar métrica de complexidade
 - [ ] Criar casos de testes
 - [ ] Verificar Resultados no JaCoCo (Relatório)
+
+## Referencias
--- contratacaoFuncionario.java
+++ contratacaoFuncionario.java
@@ -5,31 +5,28 @@ public class ContratacaoFuncionario {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-/*------[1]------*/
+    /*[1]: Coleta da idade do funcionário*/
 
     System.out.println("Informe a idade do funcionário: ");
     int idade = scanner.nextInt();
 
-/*------[2]------*/
+    /*[2]: Caso tenha menos de 16 anos de idade*/
 
     if (idade < 16) {
       System.out.println(
         "A empresa não contrata funcionários com menos de 16 anos."
       );
-
-/*------[3]------*/
+      /*[3]: Caso tenha entre 16 e 18 anos de idade*/
 
     } else if (idade >= 16 && idade <= 18) {
       System.out.println("O funcionário pode ser contratado em tempo parcial.");
-
-/*------[4]------*/
+      /*[4]: Caso tenha entre 19 e 55 anos de idade*/
 
     } else if (idade >= 19 && idade <= 55) {
       System.out.println(
         "O funcionário pode ser contratado em período integral."
       );
-
-/*------[5]------*/
+      /*[5]: Caso tenha mais de 55 anos de idade*/
 
     } else {
       System.out.println(
@@ -37,7 +34,7 @@ public class ContratacaoFuncionario {
       );
     }
 
-/*------[6]------*/
+    /*[6]: Fim do Programa*/
 
     scanner.close();
   }
